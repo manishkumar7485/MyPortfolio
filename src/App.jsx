1424d717efebc264b22b12b554b954b2ea0cbbf1
manishkumar7485 <manishsah7485@gmail.com>
2026-08-12 23:53:25 +0530

    modified code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import {
   Mail, Phone, CalendarDays, MapPin, ChevronDown, Github, Linkedin,
   Instagram, Facebook, Twitter, Code2, Palette, Monitor, Send,
